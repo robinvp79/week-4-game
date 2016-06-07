62a01ff870d4eb50b26d06271c98c3a061a1f0ce
Robin Valladolid <Robin@Robins-Air.fios-router.home>
2016-06-07 14:56:48 -0400

    game finished

---
+++ assets/javascript/game.js
@@ -0,0 +1,75 @@
+$( document ).ready(function() {
+    
+    $('.crystalImage').on('click', function(){
+      counter = counter + parseInt($(this).data('num'));
+      
+      $('#yourNumber').text(counter);
+      if (counter == numberToGuess){
+        alert('You won!!!!');
+      }else if( counter > numberToGuess){
+        alert('You lost!');
+      }
+    });
+
+    var numberToGuess = Math.floor(Math.random() * 101) + 19;
+    var counter = 0;
+    var redNumber = Math.floor(Math.random() * 11) + 1;
+    var blueNumber = Math.floor(Math.random() * 11) + 1;
+    var yellowNumber = Math.floor(Math.random() * 11) + 1;
+    var greenNumber = Math.floor(Math.random() * 11) + 1;
+    var winGames = 0;
+    var lossGames = 0;
+
+    $('#compNumber').text(numberToGuess);
+    $('#playerScore').text(counter);
+
+    $('#red').on('click',function(){
+      counter = counter + redNumber;
+      $('#playerScore').text(counter);
+      checkWin();
+    })
+
+    $('#blue').on('click',function(){
+      counter = counter + blueNumber;
+      $('#playerScore').text(counter);
+      checkWin();
+    })    
+
+    $('#yellow').on('click',function(){
+      counter = counter + yellowNumber;
+      $('#playerScore').text(counter);
+      checkWin();
+    })
+
+    $('#green').on('click',function(){
+      counter = counter + greenNumber;
+      $('#playerScore').text(counter);
+      checkWin();
+    })
+
+    function checkWin() {
+      if (counter == numberToGuess){
+        $('#message').text('You Won!');
+        winGames ++;
+        $('#wins').text('Wins: ' + winGames);
+        resetGame();
+      }else if( counter > numberToGuess){
+        $('#message').text('You Lost!');
+        lossGames ++;
+        $('#losses').text('Losses: ' + lossGames);
+        resetGame();
+      }
+    }
+
+    function resetGame() {
+      numberToGuess = Math.floor(Math.random() * 101) + 19;
+      counter = 0;
+      redNumber = Math.floor(Math.random() * 11) + 1;
+      blueNumber = Math.floor(Math.random() * 11) + 1;
+      yellowNumber = Math.floor(Math.random() * 11) + 1;
+      greenNumber = Math.floor(Math.random() * 11) + 1;
+      $('#compNumber').text(numberToGuess);
+      $('#playerScore').text(counter);
+    }
+
+  });
